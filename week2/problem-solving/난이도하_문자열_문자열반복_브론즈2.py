d90cce8a25ad8697675cd7d341db7c0f32793b57
reumkoo <reum.celeste.koo@gmail.com>
2026-03-09 23:16:50 +0900

    하: 문자열_문자열반복_브론즈2 코드 생성

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,16 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+T = int(input())
+testCase = [list(map(str, input().split())) for _ in range(T)]
+
+
+def print_number(i, R, S):
+    if i == len(S):
+        return
+    print(S[i] * int(R), end="")
+    print_number(i+1, R, S)
+
+for j in range(len(testCase)):
+    print_number(0, testCase[j][0], testCase[j][1])
+    print("")
