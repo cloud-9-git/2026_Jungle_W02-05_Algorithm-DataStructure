81d4ba69282cc6475999519d28401c31bdc578ed
reumkoo <reum.celeste.koo@gmail.com>
2026-03-09 16:29:40 +0900

    하:파이썬문법_최대값_브론즈3 코드 입력

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,13 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+nums = [int(input()) for _ in range(9)]
+
+max_number = 0
+max_number_index = 0
+for i in range(len(nums)):
+    if nums[i] >= max_number:
+        max_number = nums[i]
+        max_number_index = i +  1
+
+print(max_number)
+print(max_number_index)
