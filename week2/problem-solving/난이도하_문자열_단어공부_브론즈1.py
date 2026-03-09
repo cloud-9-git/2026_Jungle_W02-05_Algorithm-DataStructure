b493b662bdce784561ebc2f69ef33a96a93a4fff
reumkoo <reum.celeste.koo@gmail.com>
2026-03-09 23:49:24 +0900

    하: 문자열 단어공부 브론즈1 코드 작성

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,27 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+word = input()
+upper_word = word.upper()
+
+seen = {}
+result = []
+
+for char in upper_word:
+    if char not in seen:
+        seen[char] = 1
+        continue
+    elif char in seen:
+        seen[char] = seen[char] + 1
+max_count = max(seen.values())
+
+for key, value in seen.items():
+    if value == max_count:
+        result.append(key)
+
+if len(result) == 1:
+    print(result[0])
+else:
+    print('?')
+        
+
