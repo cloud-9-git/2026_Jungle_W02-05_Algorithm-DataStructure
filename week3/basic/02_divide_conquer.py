264a8b36882c4219b177004b1b7cd98d560d37c4
reumkoo <reum.celeste.koo@gmail.com>
2026-03-13 14:46:06 +0900

    Basic 02: Divide and Conquer 코드 구현

--- week3/basic/02_divide_conquer.py
+++ week3/basic/02_divide_conquer.py
@@ -36,19 +36,20 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
-    
+    if left == right:
+        return arr[left]
     # TODO: 중간 지점 계산
-    pass
-    
-    # TODO: 왼쪽 절반의 최댓값
-    pass
+    else:
+        mid = (left+right) // 2
+        # TODO: 왼쪽 절반의 최댓값
+        left_half_max = find_max_divide_conquer(arr, left, mid)
+            
+        # TODO: 오른쪽 절반의 최댓값
+        right_half_max = find_max_divide_conquer(arr, mid+1, right)
         
-    # TODO: 오른쪽 절반의 최댓값
-    pass
+        # TODO: 둘 중 큰 값 반환
+        return max(left_half_max, right_half_max)
     
-    # TODO: 둘 중 큰 값 반환
-    pass
 
 # 테스트 케이스
 if __name__ == "__main__":
