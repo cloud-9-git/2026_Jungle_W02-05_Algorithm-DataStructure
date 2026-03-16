1f61b8407087d6ccada76f8961fcd540b4fdb1cc
reumkoo <reum.celeste.koo@gmail.com>
2026-03-17 02:41:19 +0900

    하(실버4): 이분탐색 - 수찾기 >> 코드 작성

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,24 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+N = int(input())
+numbs = list(map(int, input().split()))
+M = int(input())
+checks = list(map(int, input().split()))
+
+numbs.sort()
+
+def check_numb(numbs, check, left, right):
+
+    if left > right:
+        return 0
+    mid = (left+right) // 2
+    if check == numbs[mid]:
+        return 1
+    elif check < numbs[mid]:
+        return check_numb(numbs, check, left, mid - 1)
+    else:
+        return check_numb(numbs, check, mid + 1, right)
+
+for check in checks:
+    print(check_numb(numbs, check, 0, len(numbs)-1))
+
