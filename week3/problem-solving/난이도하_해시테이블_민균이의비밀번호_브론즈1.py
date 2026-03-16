e4a18be3be7b0e846a7f8d29225733f3da322833
reumkoo <reum.celeste.koo@gmail.com>
2026-03-17 00:54:40 +0900

    난이도하: 해시테이블 - 코드 작성

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,13 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+N = int(input())
+words = [list(map(str, input())) for _ in range(N)]
+
+def find_password(words):
+    for word in words:
+        reversed_word = word[::-1]
+        if reversed_word in words:
+            print(len(word), word[len(word)//2])
+            return
+            
+find_password(words)
