791fe07670250105dbfd4cc79e819452906306ba
reumkoo <reum.celeste.koo@gmail.com>
2026-03-17 02:56:59 +0900

    하(실버4): 스택 - 스택 >> 코드 작성

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,26 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+N = int(input())
+commands = [list(map(str, input().split())) for _ in range(N)]
+
+stack = []
+for command in commands:
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
